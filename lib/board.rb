50cc585024d79f1d56f55451cbf53296cc34dffa
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-07-01 15:12:10 -0700

    file I/O, rows

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,23 @@
 class Board
+  attr_reader :input
+
+  def initialize(input)
+    @input = input
+  end
+
+  def rows
+    rows = input.split("\n").map(&:chars)
+    rows.each do |row|
+      row.map!(&:to_i)
+      fill_in_row(row)
+    end
+  end
+
+  private
+
+    def fill_in_row(row)
+      until row.length == 9
+        row << 0
+      end
+    end
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -1,6 +1,23 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './board'
+require_relative '../lib/board'
 
 class BoardTest < MiniTest::Test
+  def test_rows
+    board = Board.new(given_input)
+    result = [[8, 0, 0, 5, 0, 4, 0, 0, 7],
+              [0, 0, 5, 0, 3, 0, 9, 0, 0],
+              [0, 9, 0, 7, 0, 1, 0, 6, 0],
+              [1, 0, 3, 0, 0, 0, 2, 0, 8],
+              [0, 4, 0, 0, 0, 0, 0, 5, 0],
+              [2, 0, 7, 8, 1, 3, 6, 0, 4],
+              [0, 3, 0, 9, 0, 2, 0, 8, 0],
+              [0, 0, 2, 0, 7, 0, 5, 0, 0],
+              [6, 0, 0, 3, 0, 5, 0, 0, 1]]
+    assert_equal(result, board.rows)
+  end
+
+  def given_input
+    "8  5 4  7\n  5 3 9\n 9 7 1 6\n1 3   2 8\n 4     5\n2 78136 4\n 3 9 2 8\n  2 7 5\n6  3 5  1\n"
+  end
 end
