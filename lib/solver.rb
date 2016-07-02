3649a1ad1980b5c02b86ff230d3fac984157ba16
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-07-01 17:13:51 -0700

    returns values of other cells in same column

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,14 +1,18 @@
 class Solver
   attr_reader :board
-  
+
   def initialize(board)
-    @board = board
+    @board = board.board
   end
 
   def row_mates(cell)
   end
 
   def column_mates(cell)
+    mates = board.select do |element|
+      element.column == cell.column
+    end
+    mates.map(&:value)
   end
 
   def square_mates(cell)
--- test/solver_test.rb
+++ test/solver_test.rb
@@ -14,6 +14,8 @@ class SolverTest < MiniTest::Test
     "8  5 4  7\n  5 3 9\n 9 7 1 6\n1 3   2 8\n 4     5\n2 78136 4\n 3 9 2 8\n  2 7 5\n6  3 5  1\n"
   end
 
-  def test_cell_mates
+  def test_column_mates
+    result = [7, 0, 0, 8, 0, 4, 0, 0, 1]
+    assert_equal(result, @solver.column_mates(@solver.board[17]))
   end
 end
